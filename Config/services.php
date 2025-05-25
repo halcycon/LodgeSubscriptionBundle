41798a5bd69f65c9d1c0acea4a7b0509a63cb7cb
halcycon <adam@thecuckoocamp.co.uk>
2025-05-25 17:55:26 +0200

    Refactor service configuration in services.php to utilize autowiring for the LodgeSubscriptionBundle, removing manual service registrations and aliases for improved maintainability. Simplify LodgeSubscriptionIntegration class by eliminating unused configuration methods.

--- Config/services.php
+++ Config/services.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symfony\Component\DependencyInjection\Reference;
 
 return function (ContainerConfigurator $configurator) {
     $services = $configurator->services()
@@ -12,31 +11,7 @@ return function (ContainerConfigurator $configurator) {
         ->autoconfigure()
         ->public();
 
-    // Register core models
-    $services->set(\MauticPlugin\LodgeSubscriptionBundle\Model\SubscriptionModel::class)
-        ->args([
-            '$entityManager' => new Reference('doctrine.orm.entity_manager'),
-            '$fieldModel' => new Reference('mautic.lead.model.field'),
-        ]);
-        
-    $services->set(\MauticPlugin\LodgeSubscriptionBundle\Model\PaymentModel::class)
-        ->args([
-            '$entityManager' => new Reference('doctrine.orm.entity_manager'),
-        ]);
-        
-    $services->set(\MauticPlugin\LodgeSubscriptionBundle\Model\YearEndModel::class)
-        ->args([
-            '$entityManager' => new Reference('doctrine.orm.entity_manager'),
-            '$fieldModel' => new Reference('mautic.lead.model.field'),
-        ]);
-    
-    // Register the main integration
-    $services->set('mautic.integration.lodgesubscription', \MauticPlugin\LodgeSubscriptionBundle\Integration\LodgeSubscriptionIntegration::class)
-        ->tag('mautic.integration')
-        ->tag('mautic.basic_integration');
-    
-    // Backwards compatibility aliases
-    $services->alias('lodge_subscription.model.subscription', \MauticPlugin\LodgeSubscriptionBundle\Model\SubscriptionModel::class);
-    $services->alias('lodge_subscription.model.payment', \MauticPlugin\LodgeSubscriptionBundle\Model\PaymentModel::class);
-    $services->alias('lodge_subscription.model.yearend', \MauticPlugin\LodgeSubscriptionBundle\Model\YearEndModel::class);
+    // Load all classes in the bundle for autowiring
+    $services->load('MauticPlugin\\LodgeSubscriptionBundle\\', '../')
+        ->exclude('../{Entity,Tests,Migrations}');
 }; 
--- Integration/LodgeSubscriptionIntegration.php
+++ Integration/LodgeSubscriptionIntegration.php
@@ -4,10 +4,8 @@ namespace MauticPlugin\LodgeSubscriptionBundle\Integration;
 
 use Mautic\IntegrationsBundle\Integration\BasicIntegration;
 use Mautic\IntegrationsBundle\Integration\Interfaces\BasicInterface;
-use Mautic\IntegrationsBundle\Integration\Interfaces\ConfigFormInterface;
-use MauticPlugin\LodgeSubscriptionBundle\Form\Type\ConfigIntegrationType;
 
-class LodgeSubscriptionIntegration extends BasicIntegration implements BasicInterface, ConfigFormInterface
+class LodgeSubscriptionIntegration extends BasicIntegration implements BasicInterface
 {
     public const NAME = 'LodgeSubscription';
     
@@ -24,32 +22,5 @@ class LodgeSubscriptionIntegration extends BasicIntegration implements BasicInte
     public function getIcon(): string
     {
         return 'plugins/LodgeSubscriptionBundle/Assets/img/icon.png';
-    }
-
-    public function getConfigFormName(): string
-    {
-        return ConfigIntegrationType::class;
-    }
-
-    /**
-     * Return the content template for the form.
-     */
-    public function getConfigFormContentTemplate(): ?string
-    {
-        return null;
-    }
-
-    /**
-     * Return default configuration for the integration.
-     */
-    public function getDefaultConfigFormData(): array
-    {
-        return [
-            'standard_subscription_amount' => 250.00,
-            'senior_subscription_amount' => 125.00,
-            'stripe_webhook_secret' => '',
-            'payment_success_url' => '',
-            'payment_cancel_url' => '',
-        ];
     }
 } 
